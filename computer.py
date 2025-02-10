b0a77557336b474e3e969222cd7454fdcf1cc2b1
Kimaya Mehta <krmehta@smith.edu>
2025-02-09 23:28:42 -0500

    - edited files to use object oriented approach - added functionality to computer class - added functionality to resale shop class

--- computer.py
+++ computer.py
@@ -1,10 +1,28 @@
 class Computer:
 
-    # What attributes will it need?
+    # the Computer class has the following attributes
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # the constructor creates a new Computer instance with the given attributes
+    def __init__(self, desc:str, processor:str, capacity:int, mem:int, OS:str, year:int, pr:int):
+        self.description = desc
+        self.processor_type = processor
+        self.hard_drive_capacity = capacity
+        self.memory = mem
+        self.operating_system = OS
+        self.year_made = year
+        self.price = pr
+    
+    # the update_com method updates the computer's operating system to the given OS
+    def update_com(self, new_os: str):
+        self.operating_system = new_os
 
-    # What methods will you need?
+    # the print_com method returns a string representation of the computer's details
+    def print_com(self):
+        return(f'description: {self.description}, processor_type: {self.processor_type}, hard_drive_capacity: {str(self.hard_drive_capacity)}, memory: {str(self.memory)}, operating_system: {self.operating_system}, year_made: {str(self.year_made)}, price: {str(self.price)}')
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,74 @@
+from computer import *
+
+# the ResaleShop class represents a resale shop that buys and sells computers
+# I have moved the print statements from the main file to the relevant functions in order to simplify the functionality
 class ResaleShop:
 
-    # What attributes will it need?
+    # the ResaleShop object has the following attributes
+    inventory : list = [] # list of computers
+    name: str # name of the shop
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # the constructor creates a new ResaleShop instance and prints out the given name
+    def __init__(self, name: str):
+        self.name = name
+        print("-" * 21)
+        print(self.name)
+        print("-" * 21) 
 
-    # What methods will you need?
+    # the buy method adds the computer to the shop inventory and returns its index
+    def buy(self, computer:Computer):
+            print(f"Buying {getattr(computer, "description")}")
+            print("Adding to inventory...")
+            new = computer
+            self.inventory.append(new)
+            print("Done.\n")
+            return self.inventory.index(new)
+
+    # the update_price method updates the computer's price to the given price
+    def update_price(self, item_id: int, new_price: int):
+        if self.inventory[item_id] is not None:
+            print(f"Updating price for Item ID: {item_id} to {new_price}")
+            temp = self.inventory[item_id]
+            temp.price = new_price
+            print("Done.\n")
+        else:
+            print("Item", item_id, "not found. Cannot update price.") # error message if the item is not found
+
+    # the refurbish method updates the computer's operating system as well as price based on the year of production
+    def refurbish(self, item_id: int, new_os: str): 
+        if self.inventory[item_id] is not None:
+            print(f"Refurbishing Item ID: {item_id}, updating OS to {new_os}")
+            print("Updating inventory...")
+            temp = self.inventory[item_id]
+            temp.update_com(new_os) # calls the update_com method from the Computer class to update the operating system
+            if temp.year_made < 2000: # the price attribute is updated based on the year of production
+                temp.price = 0
+            elif temp.year_made < 2012:
+                temp.price = 250
+            elif temp.year_made < 2018:
+                temp.price = 550
+            else:
+                temp.price = 1000
+            print("Done.\n")
+        else:
+            print("Item", item_id, "not found. Cannot refurbish.") # error message if the item is not found
+
+    # the sell method removes the computer from the inventory
+    def sell(self, item_id: int):
+        if self.inventory[item_id] is not None:
+            print(f"Selling Item ID: {item_id}")
+            self.inventory.pop(item_id)
+            print(f"Item {item_id} sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.") # error message if the item is not found
+
+    # the print_inventory method prints out the inventory
+    def print_inventory(self):
+        if self.inventory:
+            print("Checking inventory...")
+            for item in self.inventory:
+                print(f"Item ID: {self.inventory.index(item)} : {item.print_com()}") # calls the print_com method from the Computer class to print out each computer's details
+            print("Done.\n")
+        else:
+            print("No inventory to display.") # error message if the inventory is empty
+
